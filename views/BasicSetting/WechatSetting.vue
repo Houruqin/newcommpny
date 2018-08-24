1f412b80c4a0a76f334995e92395009986116ba3
sonamu <sonamu@sonamudeMacBook-Pro.local>
2018-08-24 14:44:24 +0800

    fix

--- views/BasicSetting/WechatSetting.vue
+++ views/BasicSetting/WechatSetting.vue
@@ -1,275 +1,87 @@
-<template>
-    <div class="flex1 alarm_setting">
-        <el-card shadow="hover">
-            <TableHeader title="微信设置"></TableHeader>
-            <el-row>
-                <el-col class="student_alarm" :span="9" :offset="1">
-                    <el-row>
-                        <el-col>学员提醒</el-col>
-                        <el-col></el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>购买成功通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentCourse.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>分班通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentGrade.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>排课通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.timetableStudent.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row class="input_row">
-                        <el-col>上课提醒</el-col>
-                        <el-col>
-                            <div>
-                                <el-switch v-model="setting.studentLessonRemind.status">
-                                </el-switch>
-                            </div>
-                            <div>
-                                <label>提前多少小时</label>
-                                <el-input @change="show_button.studentLessonRemind = true" :disabled="!setting.studentLessonRemind.status" v-model="setting.studentLessonRemind.num" size="small"></el-input>
-                                <div v-if="show_button.studentLessonRemind" class="button">保存</div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>课时消耗提醒</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentLessonEnd.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>请假审核通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentLeaveTicketProcessRemind.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>课程变更通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentTimetableChange.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>课程取消通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentTimetableCancel.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row class="input_row">
-                        <el-col>学费即将到期提醒</el-col>
-                        <el-col>
-                            <div>
-                                <el-switch v-model="setting.studentLessonRemainRemind.status">
-                                </el-switch>
-                            </div>
-                            <div>
-                                <label>剩余多少小时</label>
-                                <el-input v-model="setting.studentLessonRemainRemind.num" size="small"></el-input>
-                                <div class="button">保存</div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>课程结课通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.studentLessonEndRemind.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                </el-col>
-                <el-col class="staff_alarm" :span="9" :offset="3">
-                    <el-row>
-                        <el-col>员工提醒</el-col>
-                        <el-col></el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>老师-排课通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.timetableTeacher.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row class="input_row">
-                        <el-col>老师-上课提醒</el-col>
-                        <el-col>
-                            <div>
-                                <el-switch v-model="setting.teacherLessonRemind.status">
-                                </el-switch>
-                            </div>
-                            <div>
-                                <label>提前多少小时</label>
-                                <el-input v-model="setting.teacherLessonRemind.num" size="small"></el-input>
-                                <div class="button">保存</div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                    <el-row class="input_row">
-                        <el-col>老师-上课结课提醒</el-col>
-                        <el-col>
-                            <div>
-                                <el-switch v-model="setting.teacherLessonEndReminding.status">
-                                </el-switch>
-                            </div>
-                            <div>
-                                <label>课后多少小时</label>
-                                <el-input v-model="setting.teacherLessonEndReminding.num" size="small"></el-input>
-                                <div class="button">保存</div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>老师-学员请假提醒</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.teacherLeaveTicketRemind.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>老师-课程变更通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.teacherTimetableChange.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>老师-课程取消通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.teacherTimetableCancel.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row class="input_row">
-                        <el-col>老师-学员学费即将到期提醒</el-col>
-                        <el-col>
-                            <div>
-                                <el-switch v-model="setting.teacherStudentLessonRemainRemind.status">
-                                </el-switch>
-                            </div>
-                            <div>
-                                <label>剩余多少小时</label>
-                                <el-input v-model="setting.teacherStudentLessonRemainRemind.num" size="small"></el-input>
-                                <div class="button">保存</div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>顾问-学员分配提醒</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.sellerStudentDistribute.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                    <el-row class="input_row">
-                        <el-col>老师-客户跟进提醒</el-col>
-                        <el-col>
-                            <div>
-                                <el-switch v-model="setting.sellerFollowUpReminding.status">
-                                </el-switch>
-                            </div>
-                            <div>
-                                <label>提前多少小时</label>
-                                <el-input v-model="setting.sellerFollowUpReminding.num" size="small"></el-input>
-                                <div class="button">保存</div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col>顾问-签约成功通知</el-col>
-                        <el-col>
-                            <el-switch v-model="setting.sellerStudentSign.status">
-                            </el-switch>
-                        </el-col>
-                    </el-row>
-                </el-col>
-            </el-row>
-        </el-card>
-    </div>
+<template lang="pug">
+  .flex1.alarm_setting
+    el-card(shadow="hover")
+      TableHeader(title="微信设置")
+      el-row
+
+        //- 左侧设置列表
+        el-col.student_alarm(:span="9" :offset="1")
+          el-row #[el-col 学员提醒]#[el-col]
+
+          el-row(v-for="(name, index) in LEFT_SETTING_SORT" :key="index" :class="{ input_row: 'num' in setting[name] }")
+            el-col {{ setting[name].label }}
+            el-col
+              el-switch(v-model="setting[name].status")
+              div(v-if="'num' in setting[name]")
+                label {{ setting[name].prefix }}
+                el-input(v-model="setting[name].num" size="small" :disabled="!setting[name].status")
+                .button(:class="{ disabled: !setting[name].status }") 保存
+
+        //- 右侧设置列表
+        el-col.student_alarm(:span="9" :offset="3")
+          el-row #[el-col 员工提醒]#[el-col]
+
+          el-row(v-for="(name, index) in RIGHT_SETTING_SORT" :key="index" :class="{ input_row: 'num' in setting[name] }")
+            el-col {{ setting[name].label }}
+            el-col
+              el-switch(v-model="setting[name].status")
+              div(v-if="'num' in setting[name]")
+                label {{ setting[name].prefix }}
+                el-input(v-model="setting[name].num" size="small" :disabled="!setting[name].status")
+                .button(:class="{ disabled: !setting[name].status }") 保存
 </template>
 <script>
 import TableHeader from "../../components/common/TableHeader";
 
+// 设置项的顺序
+const LEFT_SETTING_SORT = ['studentCourse', 'studentGrade', 'timetableStudent', 'studentLessonRemind', 'studentLessonEnd', 'studentLeaveTicketProcessRemind', 'studentTimetableChange', 'studentTimetableCancel', 'studentLessonRemainRemind', 'studentLessonEndRemind'];
+const RIGHT_SETTING_SORT = ['timetableTeacher', 'teacherLessonRemind', 'teacherLessonEndReminding', 'teacherLeaveTicketRemind', 'teacherTimetableChange', 'teacherTimetableCancel', 'teacherStudentLessonRemainRemind', 'sellerStudentDistribute', 'sellerFollowUpReminding', 'sellerStudentSign'];
+
 export default {
   data() {
     return {
+      LEFT_SETTING_SORT,
+      RIGHT_SETTING_SORT,
       setting: {
-          //学员-购买通知
-          studentCourse: {num: 0,status: 0},
-          //学员-分班通知
-          studentGrade: {num: 0,status: 0},
-          //学员-排课通知
-          timetableStudent: {num: 0,status: 0},
-          //学员-上课提醒
-          studentLessonRemind: {num: 0,status: 0},
-          //学员-课时消耗提醒
-          studentLessonEnd: {num: 0,status: 0},
-          //学员-请假审核通知
-          studentLeaveTicketProcessRemind: {num: 0,status: 0},
-          //学员-修改课表
-          studentTimetableChange: {num: 0,status: 0},
-          //学员-课程取消
-          studentTimetableCancel: {num: 0,status: 0},
-          //学员-学费即将到期提醒
-          studentLessonRemainRemind: {num: 0,status: 0},
-          //学员-结课通知
-          studentLessonEndRemind: {num: 0,status: 0},
-          //教师-排课通知
-          timetableTeacher: {num: 0,status: 0},
-          //老师-上课提醒
-          teacherLessonRemind: {num: 0,status: 0},
-          //老师-课程结束未结课时间提醒
-          teacherLessonEndReminding: {num: 0,status: 0},
-          //教师-学员请假提醒
-          teacherLeaveTicketRemind: {num: 0,status: 0},
-          //老师-修改课表
-          teacherTimetableChange: {num: 0,status: 0},
-          //教师-课程取消
-          teacherTimetableCancel: {num: 0,status: 0},
-          //老师-学员剩余课时提醒
-          teacherStudentLessonRemainRemind: {num: 0,status: 0},
-          //顾问-学员分配提醒
-          sellerStudentDistribute: {num: 0,status: 0},
-          //顾问-客户跟进提醒
-          sellerFollowUpReminding: {num: 0,status: 0},
-          //顾问-签约成功通知
-          sellerStudentSign: {num: 0,status: 0},
-      },
-      show_button: {
-          studentLessonRemind: false,
-        //   studentLessonRemind: false,
-        //   studentLessonRemind: false,
-        //   studentLessonRemind: false,
+          // 有num属性表示有input输入框
+          studentCourse: { status: 0, label: '学员-购买通知'},
+          studentGrade: { status: 0, label: '学员-分班通知' },
+          timetableStudent: { status: 0, label: '学员-排课通知' },
+          studentLessonRemind: { status: 0, label: '学员-上课提醒', num: 0, prefix: '提前多少小时' },
+          studentLessonEnd: { status: 0, label: '学员-课时消耗提醒' },
+          studentLeaveTicketProcessRemind: { status: 0, label: '学员-请假审核通知' },
+          studentTimetableChange: { status: 0, label: '学员-修改课表' },
+          studentTimetableCancel: { status: 0, label: '学员-课程取消' },
+          studentLessonRemainRemind: { status: 0, label: '学员-学费即将到期提醒', num: 0, prefix: '剩余多少小时' },
+          studentLessonEndRemind: { status: 0, label: '学员-结课通知' },
+          timetableTeacher: { status: 0, label: '教师-排课通知' },
+          teacherLessonRemind: { status: 0, label: '老师-上课提醒', num: 0, prefix: '提前多少小时' },
+          teacherLessonEndReminding: { status: 0, label: '老师-课程结束未结课时间提醒', num: 0, prefix: '课后多少小时' },
+          teacherLeaveTicketRemind: { status: 0, label: '教师-学员请假提醒' },
+          teacherTimetableChange: { status: 0, label: '老师-修改课表' },
+          teacherTimetableCancel: { status: 0, label: '教师-课程取消' },
+          teacherStudentLessonRemainRemind: { status: 0, label: '老师-学员剩余课时提醒', num: 0, prefix: '剩余多少小时' },
+          sellerStudentDistribute: { status: 0, label: '顾问-学员分配提醒' },
+          sellerFollowUpReminding: { status: 0, label: '顾问-客户跟进提醒', num: 0, prefix: '提前多少小时' },
+          sellerStudentSign: { status: 0, label: '顾问-签约成功通知' },
       }
     };
   },
   methods: {
-    get_alarm_setting_info() {
-      this.$$request.get("api/school/weixinRemindSetLists").then(res => {
-        this.setting = res.datas;
-        let result = res.datas;
-        Object.keys(result).forEach(i => {
-            result[i].status = Boolean(result[i].status)
-        })
+    async getWechatSettings() {
+      let { datas } = await this.$$request.get('school/weixinRemindSetLists') || {};
+
+      if (!datas) return void 0;
+
+      Object.keys(this.setting).forEach(v => {
+        this.setting[v].status = !!datas[v].status;
+        this.setting[v].label = datas[v].description;
+        if ('num' in this.setting[v]) this.setting[v].num = datas[v].num;
       });
     }
   },
   created() {
-    this.get_alarm_setting_info();
+    this.getWechatSettings();
   },
   components: {
     TableHeader
@@ -335,6 +147,9 @@ export default {
               background-color: #45dad5;
               color: #fff;
               cursor: pointer;
+              &.disabled {
+                background: #dcdfe6;
+              }
           }
       }
   }
