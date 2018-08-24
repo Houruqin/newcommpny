7a579c4c5f6c3c21431b721f67c11826e2fed025
sonamu <sonamu@sonamudeMacBook-Pro.local>
2018-08-24 18:37:29 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,60 @@ const config = {
   host: '0.0.0.0'
 };
 
+function VueCssLoader (options) {
+  options = options || {};
+
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
+  // generate loader string to be used with extract text plugin
+  function generateLoaders (loader, loaderOptions) {
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      })
+    }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }
+
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  }
+}
+
 module.exports = {
   devtool: '#source-map',
   entry: {
@@ -31,7 +85,12 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.vue$/, loader: 'vue-loader', options: { extractCSS: true } },
+      { test: /\.vue$/, loader: 'vue-loader', options: {
+        loaders: VueCssLoader({
+          sourceMap: true,
+          extract: true
+        })
+      } },
       { test: /\.pug$/, loader: `pug-loader?pretty` },
       { test: /\.html$/, loader: 'html-loader' },
       { test: /\.(?:yml|yaml)$/, loader: 'json-loader!yaml-loader' },
