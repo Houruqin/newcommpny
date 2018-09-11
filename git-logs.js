79514940e3ba504090e65aa225e2d9f440918c99
sonamu <sonamu@sonamudeMacBook-Pro.local>
2018-09-11 10:22:14 +0800

    fix

--- git-logs.js
+++ git-logs.js
@@ -1,9 +1,10 @@
+const argv = require('yargs').argv;
 const { exec } = require('child_process');
 
 let date = new Date();
 date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
-exec(`git log --author sonamu --since="${date}" | sed 's/[a-zA-Z0-9[:punct:]]//g'`, (err, stdout, stderr) => {
+exec(`git log --author ${argv.u || 'sonamu'} --since="${date}" | sed 's/[a-zA-Z0-9[:punct:]]//g'`, (err, stdout, stderr) => {
   if (err) {
     return console.log(err);
   }
