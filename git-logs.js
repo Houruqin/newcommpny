0ed16e371c10fe4552e2a0ac7d53ab6b6417d435
sonamu <sonamu@sonamudeMacBook-Pro.local>
2018-09-11 11:39:53 +0800

    fix

--- git-logs.js
+++ git-logs.js
@@ -4,11 +4,9 @@ const { exec } = require('child_process');
 let date = new Date();
 date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
-exec(`git log --author ${argv.u || 'sonamu'} --since="${date}"`, (err, stdout, stderr) => {
+exec(`git log --oneline --author ${argv.u || 'sonamu'} --since="${date}"`, (err, stdout, stderr) => {
   if (err) {
     return console.log(err);
   }
-
-  let logs = stdout.split('\n');
-  console.log(logs.filter((_, i) => i > 0 && i < logs.length && logs[i - 1] === logs[i + 1]).map(v => v.trim()).join('\n'))
+  console.log(stdout.replace(/^\w{7} /mg, ''))
 });
