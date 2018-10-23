4ca3b5a42a53ca409931205543e68ba187bd39d8
tushen <tushen_kun@163.com>
2018-10-23 21:27:48 +0800

    排课冲突教室选择bug

--- components/dialog/AddTimeTable.vue
+++ components/dialog/AddTimeTable.vue
@@ -191,7 +191,7 @@
                   </el-table-column>
                   <el-table-column label="冲突教室">
                       <template slot-scope="scope">
-                          <el-select v-if="scope.row.conflict_data.reason == 2" v-model="conflict_room" :multiple="addTableType === 'multiple'">
+                          <el-select v-if="scope.row.conflict_data.reason == 2" v-model="conflict_room" :multiple="addTableType === 'multiple'" key="conflict_room">
                               <el-option v-for="(item, index) in timetableFull.class_room" :key="index" :label="item.name" :value="item.id" ></el-option>
                           </el-select>
                       </template>
