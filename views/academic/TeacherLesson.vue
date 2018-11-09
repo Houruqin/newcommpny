fbd5bdf16e6042c5c38b8fe1ace8c14fdda5e9b7
zhuxun <80836358@qq.com>
2018-11-09 09:35:55 +0800

    新增消课次数统计

--- views/academic/TeacherLesson.vue
+++ views/academic/TeacherLesson.vue
@@ -49,8 +49,12 @@
           </template>
         </el-table-column>
         <el-table-column prop="lessonTime" align="center" :render-header="lessonTimeRenderHeader"></el-table-column>
-        <el-table-column prop="eliminateTime" :render-header="removeRenderHeader" align="center"></el-table-column>
-        <el-table-column prop="attendTime" :render-header="courseRenderHeader" align="center">
+        <el-table-column :render-header="removeRenderHeader" align="center">
+          <template slot-scope="scope">
+            <span class="fc-m cursor-pointer" @click="eliminationData = scope.row;getEliminationDetail(scope.row)">{{scope.row.eliminateTime}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :render-header="courseRenderHeader" align="center">
           <template slot-scope="scope"><span class="fc-m cursor-pointer" @click="attendDetail(scope.row)">{{scope.row.attendTime}}</span></template>
         </el-table-column>
         <el-table-column prop="lessonNum" :render-header="lessonNumRenderHeader" align="center"></el-table-column>
@@ -65,7 +69,28 @@
       </el-pagination>
     </el-card>
 
-    <el-dialog title="上课详情" width="650px" center :visible.sync="dialogStatus.course" :close-on-click-modal="false">
+    <el-dialog title="学员消课次数详情" center :visible.sync="dialogStatus.elimination" :close-on-click-modal="false">
+      <p class="fs-15" v-if="eliminationDetail.info">{{eliminationDetail.info.course_name}}-{{eliminationDetail.info.teacher_name}}</p>
+      <el-table v-if="eliminationDetail.list" class="mt-20 bor-t" :data="eliminationDetail.list" v-loading="loading" show-summary :summary-method="get_sum" stripe>
+        <el-table-column label="序号" type="index" align="center"></el-table-column>
+        <el-table-column label="上课日期" prop="lessonDate" align="center"></el-table-column>
+        <el-table-column label="上课时间" prop="lessonTime" align="center"></el-table-column>
+        <el-table-column label="人数上限" prop="limit_num" align="center"></el-table-column>
+        <el-table-column label="应到人数" prop="shouldComeNum" align="center"></el-table-column>
+        <el-table-column label="超员人数" prop="moreNum" align="center"></el-table-column>
+        <el-table-column label="试听人数" prop="listenNum" align="center"></el-table-column>
+      </el-table>
+      <el-pagination v-if="eliminationDetail.total"
+          class="d-f f-j-c mt-50 mb-50"
+          :page-size="eliminationDetail.per_page"
+          background layout="total, prev, pager, next"
+          :total="eliminationDetail.total"
+          :current-page="eliminationDetail.current_page"
+          @current-change="dialogPaginationClick">
+      </el-pagination>
+    </el-dialog>
+
+    <el-dialog title="上课详情" width="800px" center :visible.sync="dialogStatus.course" :close-on-click-modal="false">
       <p class="fs-15">上课老师：{{courseDetail.teacher_name}}</p>
       <el-table class="mt-20 bor-t" :data="courseDetail.lists" v-loading="loading" stripe height="300">
         <el-table-column label="序号" type="index" align="center"></el-table-column>
@@ -75,6 +100,7 @@
         <el-table-column label="扣课时数" prop="attendLessonNum" align="center"></el-table-column>
       </el-table>
     </el-dialog>
+
   </div>
 </template>
 
@@ -92,13 +118,18 @@ export default {
       loading: false,
       activeTab: '1',
       tabHeader: [{id: '1', name: '任课老师'}, {id: '2', name: '辅助老师'}],
-      dialogStatus: {course: false},
+      dialogStatus: {
+        elimination: false,
+        course: false
+        },
       searchFilter: {
         begin_time: new Date(this.$format_date(new Date(), 'yyyy/MM/01')),
         end_time: new Date(new Date().setMonth(new Date().getMonth() + 1)).setDate(0),
         course_id: '', keyword: ''
       },
       courseDetail: {teacher_name: '', lists: []},
+      eliminationData: '',
+      eliminationDetail: '',
       lessonTable: {}
     };
   },
@@ -133,6 +164,22 @@ export default {
       }
       this.getLessonLists();
     },
+    async getEliminationDetail(data,page) {
+      const params = {
+        teacher_id: data.teacherId,
+        course_id: data.courseId,
+        grade_id: data.gradeId,
+        begin_time: this.searchFilter.begin_time/1000,
+        end_time: this.searchFilter.end_time/1000,
+        teacher_type: this.activeTab,
+        page: page || 1,
+        pageNum: 5
+      }
+      let res = await this.$$request.post('eduCount/classTimeStatisticDetail',params);
+      if(!res) return false;
+      this.dialogStatus.elimination = true;
+      this.eliminationDetail = res.lists;
+    },
     attendDetail (data) {
       this.courseDetail.teacher_name = data.teacherName;
       this.courseDetail.lists = data.attendList;
@@ -140,6 +187,9 @@ export default {
     },
     paginationClick (current_page) {
       this.getLessonLists(current_page);
+    },
+    dialogPaginationClick(current_page) {
+      this.getEliminationDetail(this.eliminationData,current_page)
     },
     //导出列表
     async exportTable () {
@@ -157,6 +207,31 @@ export default {
       };
 
       window.location.href = `${baseUrl}eduCount/export?${qs.stringify(params)}`;
+    },
+    get_sum (param) {
+      let sums = [];
+      const { columns, data } = param;
+
+      sums[1] = '总计:';
+      // sums[1] = '(该时段内的所有数据)';
+      columns.forEach((item, index) => {
+        switch (item.label) {
+          case '人数上限':
+            return sums[index] = this.eliminationDetail.info.totalLimitNum;
+            break;
+          case '应到人数':
+            return sums[index] = this.eliminationDetail.info.totalShouldComeNum;
+            break;
+          case '超员人数':
+            return sums[index] = this.eliminationDetail.info.totalMoreNum;
+            break;
+          case '试听人数':
+            return sums[index] = this.eliminationDetail.info.totalListenNum;
+            break;
+        }
+      });
+
+      return sums;
     },
     //获取课时列表
     async getLessonLists (page) {
