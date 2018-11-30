eb51419ede53412894128440ce09d8f8ab9ff62c
tushen <tushen_kun@163.com>
2018-11-30 17:22:37 +0800

    登记学员弹窗不消失问题

--- views/Home.vue
+++ views/Home.vue
@@ -395,6 +395,7 @@ export default {
     //登记成功，购课回调
     CB_buyCourse (data) {
       console.log(data);
+      this.dialogStatus.student = false;
       let params = {
         student_id: data.id,
         advisor_id: data.advisor_id,
